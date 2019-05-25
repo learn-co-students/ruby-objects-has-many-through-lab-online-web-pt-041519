00dffa741d3c78fa50852b42dfe63c8362a9edf9
Michael Chen <michaelchengaming@gmail.com>
2019-05-24 18:19:41 -0700

    Done.

---
+++ lib/doctor.rb
@@ -0,0 +1,30 @@
+require "pry"
+
+class Doctor
+
+  @@all = []
+
+  attr_accessor :name, :appointments, :patients
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_appointment(patient, date)
+    Appointment.new(date, patient, self)
+  end
+
+  def appointments
+    Appointment.all.select {|appointment| appointment.doctor == self}
+  end
+
+  def patients
+    appointments.collect{|appointment| appointment.patient}
+  end
+
+end
