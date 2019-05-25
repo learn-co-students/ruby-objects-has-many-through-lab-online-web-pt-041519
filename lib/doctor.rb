48fc1c67455f3b337dd6d0b12cc0a704049a10bf
github username <github email address>
2019-05-24 23:54:30 -0400

    Done.

---
+++ lib/doctor.rb
@@ -0,0 +1,30 @@
+class Doctor
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+
+  end
+
+  def self.all
+    @@all
+  end
+
+
+  def new_appointment(date, doctor)
+      Appointment.new(date, self, doctor )
+
+  end
+
+  def appointments
+    Appointment.all.select { |appointment| appointment.doctor == self }
+  end
+
+  def patients
+   Patient.all
+  end
+
+end
