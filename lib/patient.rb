2ca792cc27d0876f4ade611c8aa20ed4910251ce
Cody Frank <cody.frank30@gmail.com>
2019-05-26 16:02:29 +0000

    Done.

---
+++ lib/patient.rb
@@ -0,0 +1,32 @@
+require"pry"
+class Patient
+
+  attr_reader :name, :appointment, :doctor
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_appointment(doctor, date)
+    Appointment.new(date, self, doctor)
+  end
+
+  def appointments
+    Appointment.all.select do |appointment|
+      appointment.patient == self
+    end
+  end
+
+  def doctors
+    appointments.collect do |appointment|
+      appointment.doctor
+    end
+  end
+end
